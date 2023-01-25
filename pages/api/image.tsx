c2412bd96ef0b28e865545ff22f410dc6c08f47a
MayurDeopa <frostkickerzvashi@gmail.com>
2023-01-25 19:51:40 +0530

    types

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -17,9 +17,9 @@ export default async function handler(
 
   if(tags){
     let imagekit = new ImageKit({
-      publicKey:process.env.IMAGEKIT_PUBLIC_KEY ,
-      privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
-      urlEndpoint:process.env.IMAGEKIT_URL_ENDPOINT
+      publicKey:process.env.IMAGEKIT_PUBLIC_KEY || '',
+      privateKey:process.env.IMAGEKIT_PRIVATE_KEY || '',
+      urlEndpoint:process.env.IMAGEKIT_URL_ENDPOINT || ''
     })
   
     try{
