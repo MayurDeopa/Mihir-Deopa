2616e971c748af4d45799bfff1e4c9d7ca52d3b6
MayurDeopa <frostkickerzvashi@gmail.com>
2023-01-31 21:27:36 +0530

    added browser friendly animation

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -15,15 +15,11 @@ const Video:React.FC<VideoProps> =(props)=>{
 
     const {
         src,
-        alt='An image',
+        alt='A video',
     } = props
 
     const [open,setOpen] = useState<boolean>(false)
 
-    const setScroll =(value:boolean)=>{
-        const body = document.getElementsByTagName('body')[0]
-        body.style.overflowY = value?'hidden':'scroll'
-    }
 
     const handleOpen =()=>{
         setOpen(true)
@@ -35,8 +31,8 @@ const Video:React.FC<VideoProps> =(props)=>{
 
     return(
         <React.Fragment>
-            <div className={styles.image_wrapper} onClick={handleOpen}>
-                <video src={src} className={styles.video}  muted  />
+            <div className={styles.image_wrapper} onClick={handleOpen} >
+                <video src={src} className={styles.video}  />
             </div>
             <VideoDrawer open={open} action={handleClose} videoSrc={src}/>
         </React.Fragment>
